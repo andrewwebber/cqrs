c6f15d3bc7ba25a69493abdaf34ddebf1f8e68b6
andrew webber (personal) <andrewvwebber@googlemail.com>
2015-03-12 18:53:50 +0100

    Correlation error routing

---
+++ correlation.go
@@ -0,0 +1,25 @@
+package cqrs
+
+import (
+	"time"
+
+	"code.google.com/p/go-uuid/uuid"
+)
+
+const CQRSErrorEventType = "cqrs.CQRSErrorEvent"
+
+// CQRSErrorEvent is a generic event raised within the CQRS framework
+type CQRSErrorEvent struct {
+	Message string
+}
+
+func DeliverCQRSError(correlationID string, err error, repo EventSourcingRepository) {
+	repo.GetEventStreamRepository().SaveIntegrationEvent(VersionedEvent{
+		ID:            uuid.New(),
+		CorrelationID: correlationID,
+		SourceID:      "",
+		Version:       0,
+		EventType:     CQRSErrorEventType,
+		Created:       time.Now(),
+		Event:         CQRSErrorEvent{Message: err.Error()}})
+}
